5d59a13cf21cea18931318e2bf1ad588ce5a4961
Domanik.Johnson@outlook.com <Domanik.Johnson@outlook.com>
2020-03-15 00:12:11 -0400

    finished project 1/100

--- ChangeBackgroundColor/js/index.js
+++ ChangeBackgroundColor/js/index.js
@@ -1,15 +1,19 @@
 // Grad body & button element
 let bdy = document.getElementsByTagName('body')[0];
-
-
 let btn = document.getElementById('onChange')
 
+// Set event on button
 btn.addEventListener('click', changeColor);
 
+/*
+* To set random background color
+* @return None
+*/
 function changeColor() {
  let r = Math.floor(Math.random() * 255 + 1);
  let g = Math.floor(Math.random() * 255 + 1);
  let b = Math.floor(Math.random() * 255 + 1); 
 
+ // Set random background color 
  bdy.style.backgroundColor = `rgb(${r.toString()},${g.toString()},${b.toString()})`;
 }
