174a02f272060e6d27b7ab2a2e0671ed63a16ce3
Domanik.Johnson@outlook.com <Domanik.Johnson@outlook.com>
2020-03-28 12:38:26 -0400

    Added form event

--- Filter JavaScript/js/index.js
+++ Filter JavaScript/js/index.js
@@ -1,1 +1,26 @@
-alert("dfsd")
+(function() {
+
+ // Grabbing 
+ let form = document.getElementsByTagName('form')[0];
+
+
+
+
+
+form.addEventListener('submit', (event) => {
+  
+  event.preventDefault();
+  
+  let dataFilter = document.querySelectorAll('.filter-btn')
+
+  
+  console.log("onloaddd");
+  });
+
+
+
+
+
+
+
+})();
